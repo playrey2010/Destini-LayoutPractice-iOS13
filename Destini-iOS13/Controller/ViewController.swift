a4dd6893981a9f2c55daf9b795fa0bf2c51b74af
Reynaldo Aponte <playrey2010@gmail.com>
2023-02-02 17:56:45 -0800

    Finished adding basic functionality

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var choiceTwo: UIButton!
+    @IBOutlet weak var choiceOne: UIButton!
+    @IBOutlet weak var storyText: UILabel!
+    
+    var storyBrain = StoryBrain()
+
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userAnswer)
+        
+        updateUI()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+    }
+    
+    func updateUI(){
+        storyText.text = storyBrain.getStoryTitle()
+        choiceOne.setTitle(storyBrain.getChoiceOne(), for: .normal)
+        choiceTwo.setTitle(storyBrain.getChoiceTwo(), for: .normal)
     }
 
 
